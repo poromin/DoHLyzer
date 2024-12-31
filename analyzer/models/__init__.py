63e0505ae37b85a850ac9138a60a8746cf35a5a6
poromin <poromin93@gmail.com>
2024-12-31 22:19:02 +0700

    Update __init__.py

--- analyzer/models/__init__.py
+++ analyzer/models/__init__.py
@@ -2,5 +2,5 @@ import importlib
 
 
 def create_model(version, segment_size):
-    module = importlib.import_module('analyzer.models.v{}'.format(version))
+    module = importlib.import_module('models.v{}'.format(version))
     return module.create_model(segment_size)
