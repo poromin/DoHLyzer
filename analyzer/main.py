2834c3eecd67e871c4c3be81b3dab20477167127
poromin <poromin93@gmail.com>
2024-12-31 22:37:18 +0700

    Update main.py

--- analyzer/main.py
+++ analyzer/main.py
@@ -2,29 +2,12 @@
 import argparse
 import json
 import logging
-import os
-import sys
-import importlib
 
 from sklearn.metrics import classification_report, confusion_matrix
 from tensorflow.keras.callbacks import EarlyStopping
 
 import analyzer.dataset as dataset
-
-# Function to dynamically import the model based on version
-def create_model(version, segment_size):
-    # Ensure that the models directory is in the system path
-    models_path = os.path.join(os.getcwd(), 'analyzer', 'models')
-    if models_path not in sys.path:
-        sys.path.append(models_path)
-
-    # Dynamically import the correct version of the model
-    try:
-        module = importlib.import_module(f'v{version}')
-    except ModuleNotFoundError:
-        raise ImportError(f"Model v{version} not found in models directory.")
-    
-    return module.create_model(segment_size)
+from analyzer.models import create_model
 
 
 def run_model(version, segment_size):
@@ -62,19 +45,19 @@ if __name__ == '__main__':
     results = []
 
     for segment_size in range(8, 9):
-        # Load dataset based on segment size
+
         x_train, x_test, y_train, y_test = dataset.load_dataset(args.input, segment_size, use_cache=False)
 
-        # Loop over valid model versions (v1, v2, v3)
-        for model_idx in range(1, 4):  # Limited to models v1, v2, and v3
-            for _ in range(3):  # Run each model 3 times
+        for model_idx in range(1, 4):
+            for _ in range(3):
                 results.append((run_model(model_idx, segment_size), model_idx, segment_size))
 
-    # Output results to a JSON file
-    with open(args.output, 'w') as output:
-        for res, model_idx, segments in results:
-            print('=' * 20 + f' [SEG={segments}] Model {model_idx} ' + '=' * 20)
-            print(res[0])
-            print(res[1])
+    output = open(args.output, 'w')
 
-        json.dump(results, output)
+    for res, model_idx, segments in results:
+        print('=' * 20 + ' [SEG={}] Model {} '.format(segments, model_idx) + '=' * 20)
+        print(res[0])
+        print(res[1])
+
+    json.dump(results, output)
+    output.close()
