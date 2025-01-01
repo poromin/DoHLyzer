0c6656712a1da74addbce181221575273f4c6428
poromin <poromin93@gmail.com>
2025-01-01 23:56:52 +0700

    Create v6.py

---
+++ analyzer/models/v6.py
@@ -0,0 +1,16 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv1D, MaxPool1D, Flatten
+
+def create_model(segment_size):
+    model = Sequential()
+    model.add(Conv1D(filters=segment_size * 2, kernel_size=3, input_shape=(segment_size, 5), activation='relu'))
+    model.add(MaxPool1D(pool_size=2))
+    model.add(Conv1D(filters=segment_size * 4, kernel_size=3, activation='relu'))
+    model.add(MaxPool1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(segment_size * 6, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(segment_size * 2, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
