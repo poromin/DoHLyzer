f865add125117f5f2614b3a49eb784f9f42c77b2
poromin <poromin93@gmail.com>
2025-01-01 21:38:22 +0700

    Update __init__.py

--- analyzer/models/__init__.py
+++ analyzer/models/__init__.py
@@ -1,6 +1,11 @@
 import importlib
 
 def create_model(version, segment_size):
-    print(f"Trying to import model v{version}")
+    if version in [1, 2]:
+        print("Classification of DoH vs Non-DoH.")
+    elif version in [3, 4]:
+        print("Classification of Benign DoH vs Malicious DoH.")
+    else:
+        print("Invalid model version.")
     module = importlib.import_module(f'analyzer.models.v{version}')
     return module.create_model(segment_size)
